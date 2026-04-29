67d5a6e188542a604fc07d9ed058070938da6ad8
Tahina Rakototmanarivo <to.tahina@gmail.com>
2026-04-29 17:17:39 +0300

    Updated import UI layout

--- src/Admin/settings-page.php
+++ src/Admin/settings-page.php
@@ -11,6 +11,7 @@ namespace ExportImportMenuAcfData\Services;
 $css = [];
 $tab = $_GET[ 'tab' ] ?? 'default';
 
+// Show error message when no menu is selected
 if ( isset ( $_POST[ EIMAD_SELECTBOX_MENU_NAME ] ) && $_POST[ EIMAD_SELECTBOX_MENU_NAME ] === '0' ) {
     wp_admin_notice(
         __( 'Please choose a menu in the dropdown list.', 'export-import-acf-menu-data' ),
@@ -19,6 +20,14 @@ if ( isset ( $_POST[ EIMAD_SELECTBOX_MENU_NAME ] ) && $_POST[ EIMAD_SELECTBOX_ME
             'dismissible' => true
         ]
     );
+}
+
+// Import the JSON file when a file is selected
+if ( 
+    isset ( $_FILES[ EIMAD_INPUT_IMPORT_FILE_NAME ] ) 
+    && $_FILES[ EIMAD_INPUT_IMPORT_FILE_NAME ][ 'tmp_name' ] !== '' 
+) {
+    MenuService::import();
 }
 ?>
 <div class="wrap">
@@ -50,8 +59,27 @@ if ( isset ( $_POST[ EIMAD_SELECTBOX_MENU_NAME ] ) && $_POST[ EIMAD_SELECTBOX_ME
                 </div>
             </form>
         <?php } else if ( $tab === 'import' ) { ?>
-            <p>Choose a file to import a menu and all its ACF field data.</p>
-            
+            <p>Choose a JSON file to import a menu and all its ACF field data.</p>
+            <form action="<?php echo admin_url( 'admin.php?page=export-import-menu-acf-data&tab=import' ); ?>" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="action" value="<?php echo EIMAD_IMPORT_ACTION_HOOK_NAME ?>">
+                <?php wp_nonce_field( EIMAD_NONCE_ACTION, EIMAD_NONCE_NAME ); ?>
+                <div class="row">
+                    <label for="menu-name">
+                        <span>Menu name*</span>
+                        <input type="text" id="menu-name" name="new_meu_name" required>
+                    </label>
+                    <label for="menu-file">
+                        Choose a file*
+                        <input type="file" id="menu-file" name="<?php echo EIMAD_INPUT_IMPORT_FILE_NAME ?>" required>
+                    </label>
+                    <label for="menu-override" class="reverse">
+                        Override if menu already exists
+                        <input type="checkbox" id="menu-override" name="override_menu">
+                    </label>
+                    <input type="submit" name="eimad_import-menu" class="button button-primary" value="Import Menu">
+                </div>
+                <small class="required">*Required fields</small>
+            </form>    
         <?php } ?>
     </div>
 </div>
